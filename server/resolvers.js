ec25b8a903bf72e206c1a0581f769f8f8fd5bc04
Timothy Lim <timothylim.student@gmail.com>
2020-11-11 17:28:43 -0300

    Server: Feed Resolver

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,23 @@
+const db = require("./db");
+
 const resolvers = {
+  Status: {
+    user: (status) => {
+      return db.get("users").find({ _id: status.userId }).value();
+    },
+  },
+
   Query: {
     example: () => ({ _id: "1", text: "this is an exmaple" }),
+    feed: () => {
+      return db
+        .get("feed")
+        .filter(
+          (o) => o.parentStatusId === null || o.parentStatusId === undefined
+        )
+        .orderBy("publishedAt", "desc")
+        .value();
+    },
   },
 };
 
